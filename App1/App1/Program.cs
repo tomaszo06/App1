bfc6568b644a73625bbae0ccc5a0fe2794accb62
tomaszo06 <bakczysaraj388@gmail.com>
2024-01-29 19:00:09 +0100

    Projek dzien 4(Ewa30lat)

--- App1/App1/Program.cs
+++ App1/App1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+var name = "Ewa";
+string sex = "kobieta";
+var age = 30;
+
+if (sex == "kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+
+if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa lat 30");
+}
+
+if (sex != "kobieta" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni meżczyzna");
+}
